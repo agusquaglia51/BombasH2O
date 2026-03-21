d8a31809cbe37a65cd0026350301e0c820f19b23
Agustin Quagliarella <agustin.quagliarella@constructconnect.com>
2026-03-21 14:20:47 -0300

    chore(api): add cookie-parser dependency and integrate it into the main application

--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -1,10 +1,29 @@
 import { NestFactory } from '@nestjs/core';
-import { ValidationPipe } from '@nestjs/common';
 import { AppModule } from './app.module';
+import { ValidationPipe } from '@nestjs/common';
+import * as cookieParser from 'cookie-parser';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.useGlobalPipes(new ValidationPipe({ whitelist: true }));
-  await app.listen(process.env.PORT ?? 3000);
+
+  app.enableCors({
+    origin: [process.env.APP_URL, 'http://localhost:3000'],
+    credentials: true,
+    methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE'],
+  });
+
+  app.use(cookieParser.default());
+
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+      forbidNonWhitelisted: true,
+      transform: true,
+      transformOptions: {
+        enableImplicitConversion: true,
+      },
+    }),
+  );
+  await app.listen(process.env.PORT ?? 8000);
 }
 bootstrap();
